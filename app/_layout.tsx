f21f92e0af91ae49bf60beca1a9ea412f7dc2553
Ilya Pitukh <yiggfiu@gmail.com>
2026-03-31 15:33:19 +0300

    husky final test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,4 @@
 import { Stack } from "expo-router";
-import Toast from "react-native-toast-message";
-
-const showToas = () => {
-    //test 3
-    Toast.show();
-    Toast.show();
-    Toast.show();
-    Toast.show();
-};
 
 export default function RootLayout() {
     return <Stack />;
